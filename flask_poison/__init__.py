9f280ff69e5fd7e63b8da142a8ca5183c28b796d
Danilo Moura <danilolmoura@gmail.com>
2020-07-22 23:40:03 -0300

    Add main class

---
+++ flask_poison/__init__.py
@@ -0,0 +1,8 @@
+class Api(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config.setdefault('CONFIG_TEST', 'test')
